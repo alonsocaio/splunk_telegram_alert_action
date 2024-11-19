4bd65b2cd4b257dbad5f8162b5a68c79a24f1f89
Caio Alonso <caio.alonso@outlook.com.br>
2024-11-18 22:57:05 -0300

    update alert action script

--- bin/splunk_telegram_alert_action.py
+++ bin/splunk_telegram_alert_action.py
@@ -1,30 +1,27 @@
 import requests
 import sys
+import json
 
 
-class SplunkTelegramAlertAction:
-	def __init__(self, **kwargs):
-		self.params = [
-			"bot_id",
-			"summary",
-			"message",
-			"chat_id"
-		]
+def send_telegram_message(payload):
+	config = payload.get('configuration')
+	bot_id = config.get('bot_id')
+	chat_id = config.get('chat_id')
+	summary = config.get('summary')
+	message = config.get('message')
 
+	telegram_message = '<b>%s<b>\n%s' % (summary, message)
 
-def send_telegram_message(self, bot_id, payload):
-	url = ''
-	print(bot_id)
+	url = 'https://api.telegram.org/bot%s/sendMessage' % bot_id
+
+	r = requests.post(url=url, data={'chat_id': chat_id, 'text': telegram_message, 'parse_mode': 'HTML'}).json()
 
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1 and sys.argv[1] == "--execute":
-		try:
-			splunk_telegram_alert_action = SplunkTelegramAlertAction()
-			splunk_telegram_alert_action.execute()
-			sys.exit(0)
-		except Exception as e:
-			print("Unhandled exception: " + str(e), file=sys.stderr)
+		payload = json.loads(sys.stdin.read())
+		send_telegram_message(payload)
+
 else:
 	print("Unsupported execution mode (expected --execute flag)", file=sys.stderr)
 	sys.exit(1)
